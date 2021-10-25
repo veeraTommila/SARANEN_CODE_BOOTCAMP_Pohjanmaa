926bba193be4cdffb4e7af6f17c25718b31ed12b
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-10-25 10:50:28 +0300

    Added two customerNames, customerAddresses, customerZipCodes and customerCities to file Program.cs in ClassesDemo-folder.

--- Codes_2/C#_lang/ClassesDemo/Program.cs
+++ Codes_2/C#_lang/ClassesDemo/Program.cs
@@ -6,7 +6,18 @@ namespace ClassesDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string customerName = "Allu Asiakas";
+            string customerAddress = "Vattuniemenkatu 13 B 20";
+            string customerZipCode = "00210";
+            string customerCity = "Helsinki";
+
+            PrintCustomer(customerName,customerAddress,customerZipCode,customerCity);
+
+            string customerName2 = "Moe Mooa";
+            string customerAddress2 = "Vattuniemenkuja 3 A 14";
+            string customerZipCode2 = "00210";
+            string customerCity2 = "Helsinki";
+
         }
     }
 }
