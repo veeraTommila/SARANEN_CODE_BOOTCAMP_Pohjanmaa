fb7b4b7a0087ad13e09a2b74f46c7e4c4b746de9
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-10-27 11:56:05 +0300

    New file FileSum and code in its Program.cs.

--- Codes_2/DotNet/FileSum/Program.cs
+++ Codes_2/DotNet/FileSum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileSum
 {
@@ -6,7 +7,24 @@ namespace FileSum
     {
         static void Main(string[] args)
         {
-            string filename = "D:\Tiedostot\SARANEN_CODE_BOOTCAMP_Pohjanmaa\Codes_2\DotNet";
+            string filename = "D:\\Tiedostot\\SARANEN_CODE_BOOTCAMP_Pohjanmaa\\Codes_2\\DotNet\\Numbers.txt";
+            string[] lines = File.ReadAllLines(filename);
+            int sum = 0;
+            foreach (string line in lines)
+            {
+                int value = int.Parse(line);
+                sum += value;
+            }
+            /*
+            for (int index = 0; index < lines.Lenght; index ++)
+            {
+                string line lines = lines[index];
+                int value = int.Parse(line);
+                sum += value;
+            }
+            */
+
+            Console.WriteLine("Calculated sum is:" + sum);
         }
     }
 }
