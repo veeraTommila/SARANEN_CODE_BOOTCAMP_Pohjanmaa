31061bf0bd8feff157af800002f9ea37dd672dbb
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-10-27 11:33:53 +0300

    Created file FileSum in DotNet.

---
+++ Codes_2/DotNet/FileSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "D:\Tiedostot\SARANEN_CODE_BOOTCAMP_Pohjanmaa\Codes_2\DotNet";
+        }
+    }
+}
