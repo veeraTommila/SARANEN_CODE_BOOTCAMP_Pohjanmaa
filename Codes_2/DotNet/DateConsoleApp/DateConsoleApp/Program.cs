e20cf7a9035d0e3b5409ada024ec31e5668d8cc4
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-11-01 10:26:40 +0200

    Added a simple DateTime calculation demo.

---
+++ Codes_2/DotNet/DateConsoleApp/DateConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DateConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Ask the users for two dates. Then calculate the difference of these dates
+            in both days and hours.
+            */
+
+            // To read the dates from the user.
+            //DateTime date1 = ReadDateFromUser("Give me the first date: ");
+            Console.WriteLine("Give me the first date: ");
+            string dateValue = Console.ReadLine();
+            
+            //int number = int.Parse(dateValue); //Passing a string value to a number.
+            DateTime date1 = DateTime.Parse(dateValue);
+
+            Console.WriteLine("Give me another date: ");
+            dateValue = Console.ReadLine();
+            DateTime date2 = DateTime.Parse(dateValue);
+
+            /*int aa = 120;
+            int bee = 45;
+            int difference = aa - bee;
+            Dates are related to numbers.*/
+
+            // Date time calculations, substractions. Overloading minus operator.
+            TimeSpan difference = date2 - date1;
+            Console.WriteLine("Difference in days: " + difference.TotalDays);
+            Console.WriteLine("Difference in hours: " + difference.TotalHours);
+            
+            private static DateTime
+        }
+    }
+}
--- Codes_2/DotNet/Dates/Program.cs
+++ Codes_2/DotNet/Dates/Program.cs
@@ -6,7 +6,23 @@ namespace Dates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Ask the users for two dates. Then calculate the difference of these dates
+            in both days and hours.
+            */
+            string date1, date2;
+            DateTime dateTime1, dateTime2;
+            float diffDays, diffOurs;
+
+            System.Console.WriteLine("What is the date today?: ");
+            date1 = Console.ReadLine();
+            System.Console.WriteLine("What was the date yesterday?: ");
+            date2 = Console.ReadLine();
+
+            diffDays = (date)
+
+            
+            //System.Console.WriteLine(DateTime.Now); //To get the current date and time.
         }
     }
 }
