9ce99c370877e58d1f973fb872d47c858b1629e2
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-11-05 17:38:09 +0200

    Missing key.

--- REACT/my-app/src/CustomersList.js
+++ REACT/my-app/src/CustomersList.js
@@ -5,36 +5,29 @@ class CustomerList extends React.Component {
     constructor(props) {
         super(props);
         console.log("In CustomerList.constructor");
-        this.state = {
-            customers: []
-        };
-    }; //This.state allows users to modify data on a page in real time.
-}
+        this.state = { customers: [] };
+    }
 
-componentDidMount() {
-    console.log("In CustomerList.componentDidMount");
+    componentDidMount() {
+        console.log("In CustomerList.componentDidMount");
 
-    fetch('https://localhost:44378/api/customers')
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-            this.setState({
-                customers: json
+        fetch('https://localhost:44378/api/customers')
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                this.setState({ customers: json });
+                console.log("Component state has been modified.");
             });
-            console.log("Component state has been modified.");
-        });
-    console.log("Fetch call has been made.");
-}
-myButtonClickHandler() {
-    console.log("Button was clicked!");
-    // alert("Hello from React!");
+        console.log("Fetch call has been made.");
+    }
 
-    const data = {
-        username: 'Tom Tester',
-        shoenumber: 42
-    };
-    fetch('https://localhost:44378/api/hello', {
-            method: 'POST', //Send data request back.
+    myButtonClickHandler() {
+        console.log("Button was clicked!");
+        // alert("Hello from React!");
+
+        const data = { username: 'Tom Tester', shoenumber: 42 };
+        fetch('https://localhost:44378/api/hello', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -45,47 +38,42 @@ myButtonClickHandler() {
             console.log('Success:', data.text);
             alert(data.text);
         });
-}
+    }
 
-render() {
-    //console.log("In CustomerList.render");
-
-    //let list = []; //pushing data to an array.
-    /*for (let index = 0; index < this.state.customers.length; index++) {
-        const customer = this.state.customers[index];
-        list.push(<p>Id: {customer.customerId} – Name: {customer.companyName}</p>);
-    }*/
-    console.log("In CustomerList.render");
+    render() {
+        console.log("In CustomerList.render");
 
         let list = [];
         for (let index = 0; index < this.state.customers.length; index++) {
             const customer = this.state.customers[index];
-            list.push(<tr>
+            list.push(<tr key={index}>
                 <td>{customer.customerId}</td>
                 <td>{customer.companyName}</td>
                 <td>{customer.contactName}</td>
                 <td>{customer.city}</td>
             </tr>);
-    }    
+        }
 
-    return ( <div>
-        <h1>Customer List</h1>
-        <button onClick={this.myButtonClickHandler} className="btn btn-outline-primary mt-2 mb-2">Click me!</button>
-        <p>Number of customer: {this.state.customers.length}</p>
-        <table className="table table-striped w-75">
-            <thead>
-                <tr>
-                    <th>Customer Id</th>
-                    <th>Company Name</th>
-                    <th>Contact Name</th>
-                    <th>City</th>
-                </tr>
-            </thead>
-            <tbody>
-                {list}
-            </tbody>
-        </table>
-    </div>);
-    
+        return (
+            <div>
+                <h1>Customer List</h1>
+                <button onClick={this.myButtonClickHandler} className="btn btn-outline-primary mt-2 mb-2">Click me!</button>
+                <p>Number of customer: {this.state.customers.length}</p>
+                <table className="table table-striped w-75">
+                    <thead>
+                        <tr>
+                            <th>Customer Id</th>
+                            <th>Company Name</th>
+                            <th>Contact Name</th>
+                            <th>City</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {list}
+                    </tbody>
+                </table>
+            </div>);
+    }
 }
+
 export default CustomerList;
